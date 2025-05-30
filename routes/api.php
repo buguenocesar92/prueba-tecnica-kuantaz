f2b4d40b1c319128decf8a67e374ff3840f091d5
buguenocesar92 <buguenocesar92@gmail.com>
2025-05-30 13:09:05 -0400

    Refactor API routes to update beneficios-procesados endpoint and remove auxiliary testing endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 // Rutas para la prueba técnica Kuantaz
 Route::prefix('v1')->group(function () {
-    Route::get('/beneficios-procesados', [BeneficiosController::class, 'getBeneficiosProcesados']);
-    
-    // Endpoints auxiliares para testing
-    Route::get('/beneficios', [BeneficiosController::class, 'getBeneficios']);
-    Route::get('/filtros', [BeneficiosController::class, 'getFiltros']);
-    Route::get('/fichas', [BeneficiosController::class, 'getFichas']);
+    Route::get('/beneficios-procesados', [BeneficiosController::class, 'beneficiosProcesados']);
 });
